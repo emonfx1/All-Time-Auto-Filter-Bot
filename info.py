1530e365f441eedc2c0f9625344020902b995ade
emonfx1 <153927593+emonfx1@users.noreply.github.com>
2024-05-01 12:23:38 +0600

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://dual-valli-emon-fxbot.koyeb.app/")
+URL = environ.get("URL", "https://warm-valeda-emon-fxbot.koyeb.app/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
